54af3031ca4a369fad3774db1dd2124bdc81cf95
Marat-FMK <96082935+Marat-FMK@users.noreply.github.com>
2025-01-22 19:25:29 +0300

    add mainView

--- ColorMix/ContentView.swift
+++ ColorMix/ContentView.swift
@@ -8,14 +8,45 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var selectedColorName = "Red"
+    @State private var selectedColorNameTwo = "Blue"
+    @State private var resultColor = "Purple"
+    
+    let colorNames = [ "Red", "Green", "Blue", "Yellow", "Orange", "Purple","Brown"]
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        
+        NavigationStack {
+            
+            VStack(spacing: 20) {
+                
+                Text(selectedColorName)
+                RoundedRectangle(cornerRadius: 20)
+                    .frame(width: 150, height: 150)
+                    .foregroundStyle(.red)
+                
+                
+                Text(" + ")
+                    
+                
+                Text(selectedColorNameTwo)
+                RoundedRectangle(cornerRadius: 20)
+                    .frame(width: 150, height: 150)
+                    .foregroundStyle(.blue)
+                
+                
+                Text(" = ")
+                
+                Text(resultColor)
+                RoundedRectangle(cornerRadius: 20)
+                    .frame(width: 150, height: 150)
+                    .foregroundStyle(.purple)
+                
+            }
+            .navigationTitle("ColorMix")
+            .navigationBarTitleDisplayMode(.inline)
         }
-        .padding()
     }
 }
 
---
+++ ColorMix/ViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  ViewModel.swift
+//  ColorMix
+//
+//  Created by Marat Fakhrizhanov on 22.01.2025.
+//
+
+import Foundation
