b65894d6b299fe1846fb3de8a4cb7310dcb72c79
Marat-FMK <96082935+Marat-FMK@users.noreply.github.com>
2025-01-22 18:25:13 +0300

    Initial Commit

---
+++ ColorMix/ColorMixApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorMixApp.swift
+//  ColorMix
+//
+//  Created by Marat Fakhrizhanov on 22.01.2025.
+//
+
+import SwiftUI
+
+@main
+struct ColorMixApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
